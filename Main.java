597b1de06f28f475b7f400d0ee5f076b2d754857
Pritam Halder <halderpritam1997@gmail.com>
2022-11-13 16:18:49 +0530

    Updated to expected result

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import IPL.*;
 public class Main {
     public static void main(String []args) {
         ScoreBoard s = new ScoreBoard();
-        Team[] twoConsecutiveLosses = s.getConsecutiveOutcomes(true, 2);
+        Team[] twoConsecutiveLosses = s.getConsecutiveOutcomes(false, 2);
         System.out.println("Teams: ");
         for (int i = 0; i < twoConsecutiveLosses.length; i++) {
             System.out.println(twoConsecutiveLosses[i]);
