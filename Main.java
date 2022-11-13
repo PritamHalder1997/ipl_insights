9533af9316907efca7c6b9ec4f79cc303a000963
Pritam Halder <halderpritam1997@gmail.com>
2022-11-13 16:04:48 +0530

    Main class added

---
+++ Main.java
@@ -0,0 +1,15 @@
+import IPL.*;
+
+public class Main {
+    public static void main(String []args) {
+        ScoreBoard s = new ScoreBoard();
+        Team[] twoConsecutiveLosses = s.getConsecutiveOutcomes(true, 2);
+        System.out.println("Teams: ");
+        for (int i = 0; i < twoConsecutiveLosses.length; i++) {
+            System.out.println(twoConsecutiveLosses[i]);
+        }
+        float averagePont = s.getAveragePoint(twoConsecutiveLosses);
+        System.out.println("Average Point: ");
+        System.out.println(averagePont);
+    }   
+}
